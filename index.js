2f59d3ecda465b11e2b2cb0084fd754d2cb6be8e
zooma123 <93126188+zooma123@users.noreply.github.com>
2024-03-09 17:30:21 +0200

    Socket io in progress
    Still Working in Socket Io

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const  mongoose = require("mongoose");
 const { Sequelize } = require('sequelize');
-const http = require("http");
 const url = require("url");
+const app = express();
+const http = require("http").Server(app);
+const io  = require("socket.io")(http)
 const  cors = require ("cors") ;
 const  dotenv = require ('dotenv') ;    
 const cookieParser = require('cookie-parser')
@@ -11,9 +13,9 @@ const userrouter = require('./routes/users.js')
 const roomrouter = require('./routes/room.js')
 const departmentrouter = require('./routes/department.js')
 const facultyrouter = require('./routes/faculties.js')
-
+const User = require('./models/User.js')
 dotenv.config();
-const app = express();
+
 
 app.use(cors());
  app.use(express.json())
@@ -34,6 +36,46 @@ app.use("/users" , userrouter);
 app.use("/rooms" , roomrouter);
 app.use("/department" , departmentrouter);
 app.use("/faculty" , facultyrouter);
+
+//Implementaion Of Socket Io 
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  // When a user sends their location
+  socket.on('location', async (data) => {
+      try {
+          // Find the user by username
+          const user = await User.findOne({ Username: data.Username });
+          
+          if (!user) {
+              console.log('User not found');
+              return;
+          }
+
+          // Update user's location
+          user.latitude = data.latitude;
+          user.longitude = data.longitude;
+          await user.save();
+
+          // Emit updated location to all clients
+          io.emit('locationUpdate', { Username: user.Username, latitude: user.latitude, longitude: user.longitude });
+      } catch (err) {
+          console.error('Error updating location:', err);
+      }
+  });
+
+  // Disconnect event
+  socket.on('disconnect', () => {
+      console.log('User disconnected');
+  });
+});
+
+
+
+
+
+
+
 
 
 
