d15db4188eddfad33ccb19850222ed0468e3aadc
zooma123 <93126188+zooma123@users.noreply.github.com>
2024-04-02 16:16:10 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express') ;
 const router  = express.Router();
  const  { register , login  } = require ("./../controllers/authCtl.js") 
- const {verfiytoken} = require('../utils/verfiytoken.js')
- router.post("/register" ,   register)
-router.post("/login" , verfiytoken , login)
+ const {verfiytoken , verifyStudent} = require('../utils/verfiytoken.js')
+ router.post("/register", register)
+router.post("/login" , verifyStudent , login)
 
 
 
