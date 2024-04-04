0c5c60f8b8b2f2080a74a6515c55db57162838fe
zooma123 <93126188+zooma123@users.noreply.github.com>
2024-04-05 01:48:31 +0200

    Update AdminBanel
    Ubdate AdminBanel FACULTY

--- routes/faculties.js
+++ routes/faculties.js
@@ -3,9 +3,10 @@ const express = require('express')
 const facultyrouter = express.Router()
 const facultycontroller = require('../controllers/facultyCtl.js')
 
-facultyrouter.get('/allfaculties ' , facultycontroller.getAllfaculty)
-facultyrouter.get('/:id' ,facultycontroller.findfaculty)
+facultyrouter.get('/allfaculties' , facultycontroller.getAllfaculty)
 facultyrouter.post('/addfaculty' , facultycontroller.AddFaculty)
+ facultyrouter.get('/:id' ,facultycontroller.findfaculty)
+
 facultyrouter.patch('/:id' , facultycontroller.Updatefaculty)
 facultyrouter.delete('/:id' , facultycontroller.DeleteFaculty)
 
